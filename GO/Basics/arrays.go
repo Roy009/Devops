bb00da99d64f3b4b737ae75d6e857ed6dd180bc8
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-06-27 00:41:32 +0530

    arrays, slicing and ranges

---
+++ GO/Basics/arrays.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Array Declaration
+	var ages [3]int = [3]int{20,25,30}
+	ages[1] = 34
+	fmt.Println(ages)
+	fmt.Println(len(ages))
+	name := [4]string{"Rohit","Roy","David","Ronaldo"}
+	fmt.Println(name, len(name))
+
+	// slices
+	var score = []int{100,40,50}
+	score[1] = 0
+	score = append(score, 80)
+	fmt.Println(score)
+
+	// slice Ranges
+	rangeOne := name[1 : 3]
+	rangeTwo := name[2:]
+	rangeThree := name[:3]
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+
+	rangeOne = append(rangeOne, "Vini Jr")
+	fmt.Println(rangeOne)
+}
